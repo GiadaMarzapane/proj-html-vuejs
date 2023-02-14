225bce8f7e5cbc26d30cd5a1a7444d516cc897b6
GiadaMarzapane <giada.ortesta@gmail.com>
2023-02-14 11:07:01 +0100

    Add section collection

--- src/store.js
+++ src/store.js
@@ -1,5 +1,23 @@
 import { reactive } from "vue";
 export const store = reactive({
     mainNav: ['home', 'shop', 'products', 'categories', 'news', 'elements'],
-    active: 0
+    active: 0,
+    collection: [
+        {
+            title: 'Winter Collection',
+            caption: 'stylish and warm',
+            button: 'view more',
+            img_path: '../asset/images/winter_collection_bg.jpg'
+        }, {
+            title: 'Spring Collection',
+            caption: 'bright and colorful',
+            button: 'view more',
+            img_path: '../asset/images/spring_collection_bg.jpg'
+        }, {
+            title: 'Autumn Collection',
+            caption: 'rich and comfortable',
+            button: 'view more',
+            img_path: '../asset/images/autumn_collection_bg.jpg'
+        }
+    ]
 })
